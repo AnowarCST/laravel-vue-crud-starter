24d3f83972dc1a3b40d12aee6d4a3d1a13d82728
Anowar Hossain <anowar.hossain@brainstation23.com>
2020-01-30 14:11:52 +0600

    fixing some configuration

--- config/database.php
+++ config/database.php
@@ -42,13 +42,6 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-        'testing' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
 
         'mysql' => [
             'driver' => 'mysql',
